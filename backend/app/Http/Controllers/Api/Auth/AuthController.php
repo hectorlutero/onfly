08ba2c0493d7843f83e2cc8c9ce738cc03b9b340
Hector Siman <hectorlutero@hotmail.com>
2024-04-10 17:49:22 -0300

    [FEAT] API Authentication through Sanctum implemented

---
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(AuthRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        $hash = Hash::check($request->password, $user->password);
+
+        if (!$user || !$hash) {
+            return response()->json([
+                'error' => 'Invalid Credentials'
+            ], 401);
+        }
+
+        $user->tokens()->delete();
+
+        return response()->json([
+            'token' => $user->createToken('authToken')->plainTextToken
+        ]);
+    }
+
+    public function logout(AuthRequest $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+
+    public function profile(Request $request)
+    {
+        return response()->json([
+            'user' => $request->user()
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);;
+// Route::post('/register', [AuthController::class,'register']);
+Route::get('/profile', [AuthController::class, 'profile'])->middleware(['auth:sanctum']);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
